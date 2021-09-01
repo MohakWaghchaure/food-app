e356d2706f2d88b6b77aa822fda02592a9e3e025
mohak <mohak11jan@gmail.com>
2021-09-01 22:32:33 +0530

    header and background

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props)=>{
+    const backgroundImage = 'https://images.unsplash.com/photo-1493770348161-369560ae357d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80';
+    return(
+        <Fragment>
+            <header className={classes.header}>
+                <h1>ChefBite</h1>
+                <HeaderCartButton>Cart</HeaderCartButton>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={backgroundImage} alt='delicious food' />
+            </div>
+        </Fragment>
+    )
+};
+
+export default Header;
