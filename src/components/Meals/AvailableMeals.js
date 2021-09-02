7ac70932aa80d104af5cbefea3a4c11f822c9e08
mohak <mohak11jan@gmail.com>
2021-09-02 22:57:16 +0530

    meal card and cartitem

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,28 @@
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
+const DUMMY_MEALS = [
+    {id:'001', name: 'test001', description: 'test Description 001', price: 22},
+    {id:'002', name: 'test002', description: 'test Description 002', price: 22},
+    {id:'003', name: 'test003', description: 'test Description 003', price: 22},
+    {id:'004', name: 'test004', description: 'test Description 004', price: 22},
+    {id:'005', name: 'test005', description: 'test Description 005', price: 22},
+    {id:'006', name: 'test006', description: 'test Description 006', price: 22},
+    {id:'007', name: 'test007', description: 'test Description 007', price: 22},
+]
+
+const AvailableMeals = ()=>{   
+    const mealsList = DUMMY_MEALS.map(meal=> 
+        <MealItem key={meal.id} name={meal.name} price={meal.price} description={meal.description}></MealItem>
+    ) 
+    return(
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul>                
+            </Card>
+        </section>
+    )
+}
+
+export default AvailableMeals;
